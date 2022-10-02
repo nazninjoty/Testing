0cc1e739a978e03935696286907940f562b65a2d
Naznin Akter joty <111962533+nazninjoty@users.noreply.github.com>
2022-10-02 11:57:21 +0600

    First Commit

---
+++ Test/LeapYearTest/src/TestSum.java
@@ -0,0 +1,14 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class TestSum {
+    @Test
+    public void TestSum01(){
+        App app = new App();
+        double actual = 2.2679331;
+        assertEquals(App.calculate(10, 10),actual,actual);
+
+
+    }
+}
